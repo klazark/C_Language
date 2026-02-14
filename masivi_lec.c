1fb135757e4e2a702aca2d74e5796c8a08d962d0
klazark <klazarkisk@gmail.com>
2026-02-14 20:20:19 +0200

    masivi

--- masivi_lec.c
+++ masivi_lec.c
@@ -10,26 +10,30 @@ int main()
     float s = 0, avr;
     int n, i;
 
-    do {
+    do
+    {
         printf("n=?(n<=%d):", max_br);
         scanf("%d", &n);
     } while (n < 1 || n > max_br);
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         printf("temp for day %d=", i + 1);
         scanf("%f", &temp[i]);
     }
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         s += temp[i];
     }
-    
+
     avr = s / n;
     printf("\navr=%.2f", avr);
-    
-    for (i = 0; i < n; i++) {
+
+    for (i = 0; i < n; i++)
+    {
         printf("\ndeviation for day %d=%.2f", i + 1, temp[i] - avr);
     }
-    
+
     return 0;
 }
