3647e3df7d98a0a333391f2fe5fb9a0bfff994fb
klazark <klazarkisk@gmail.com>
2026-05-12 09:56:01 +0300

    structora_uchenik.c

---
+++ struktura_uchenik.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BROJ_OCENKI 4
+
+typedef struct {
+    int nomer_v_klasa;
+    int godishni_ocenki[BROJ_OCENKI];
+    float sreden_uspeh;
+    char ime[40];
+} Uchenik;
+
+int main(void) {
+    Uchenik u;
+    int i;
+    int suma = 0;
+
+    u.nomer_v_klasa = 12;
+    strcpy(u.ime, "Maria");
+
+    u.godishni_ocenki[0] = 5;
+    u.godishni_ocenki[1] = 6;
+    u.godishni_ocenki[2] = 5;
+    u.godishni_ocenki[3] = 6;
+
+    for (i = 0; i < BROJ_OCENKI; i++) {
+        suma += u.godishni_ocenki[i];
+    }
+    u.sreden_uspeh = (float)suma / BROJ_OCENKI;
+
+    printf("Ime: %s\n", u.ime);
+    printf("Nomer v klasa: %d\n", u.nomer_v_klasa);
+    printf("Godishni ocenki: ");
+    for (i = 0; i < BROJ_OCENKI; i++) {
+        printf("%d ", u.godishni_ocenki[i]);
+    }
+    printf("\nSreden uspeh: %.2f\n", u.sreden_uspeh);
+
+    return 0;
+}
